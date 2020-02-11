8bf109f52a8b2df410fb202baec447fd6ee8c5b9
Corwin Marsh <corwinm@users.noreply.github.com>
2020-02-10 21:58:13 -0800

    Adding storybook with example

---
+++ stories/test.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ExampleComponent from '../src'
+
+export default {
+  title: 'Test',
+  component: ExampleComponent,
+};
+
+export const Testing = () => <ExampleComponent text={"testing?"} />;
