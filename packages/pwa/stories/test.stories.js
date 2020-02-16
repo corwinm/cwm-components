a7b8627b3d6e923f88a9412432400541b354dfa6
Corwin Marsh <corwinm@users.noreply.github.com>
2020-02-16 09:54:37 -0800

    Init lerna and Storybook

---
+++ packages/pwa/stories/test.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Test } from "../components/test";
+
+export default {
+  title: "Test",
+  component: Test
+};
+
+export const Text = () => <Test />;
+
+export const Second = () => <Test />;
