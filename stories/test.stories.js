fa1a22cc144213b6a239838f7b7d7c245537dad3
Corwin Marsh <corwinm@users.noreply.github.com>
2020-02-16 09:16:00 -0800

    Clean repo

--- stories/test.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ExampleComponent from '../src'
-
-export default {
-  title: 'Test',
-  component: ExampleComponent,
-};
-
-export const Testing = () => <ExampleComponent text={"testing?"} />;
