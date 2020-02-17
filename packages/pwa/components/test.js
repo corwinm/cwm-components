31d4485a559bccce8e56787e58e0c8072e8b721b
Corwin Marsh <corwinm@users.noreply.github.com>
2020-02-16 20:03:19 -0800

    Test prettier

--- packages/pwa/components/test.js
+++ packages/pwa/components/test.js
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export const Test = ({ text }) => {
-  return (
-    <div>
-      {text}
-    </div>
-  )
-}
+  return <div>{text}</div>;
+};
