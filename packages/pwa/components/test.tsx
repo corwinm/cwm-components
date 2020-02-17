ac636fc2df0f1e154debbeb8bc15c745bb87382b
Corwin Marsh <corwinm@users.noreply.github.com>
2020-02-16 21:55:31 -0800

    Test dependency on other package

--- packages/pwa/components/test.tsx
+++ packages/pwa/components/test.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { TestMessage } from "cwm-components-core";
 
 type TestProps = {
   text: String
 }
 
 export const Test: React.FC<TestProps> = ({ text }) => {
-  return <div>{text}</div>;
+  return <div>{text} {TestMessage()}</div>;
 };
